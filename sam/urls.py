54d0a9111970a89810cf167ee2c38bd65155070b
Sam Olds <samolds@uw.edu>
2013-05-11 15:20:28 -0700

    got django running very minimally

---
+++ sam/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import patterns, include, url
+
+urlpatterns = patterns('sam.views',
+    url(r'^$', 'home.home'),
+    #url(r'buildings?$', 'buildings.buildings'),
+    #url(r'search/$', 'search.SearchView'),
+    #url(r'contact(?:/(?P<spot_id>\d+))?/$', 'contact.contact'),
+    #url(r'sorry(?:/(?P<spot_id>\d+))?/$', 'contact.sorry'),
+    #url(r'thankyou(?:/(?P<spot_id>\d+))?/$', 'contact.thank_you'),
+    #url(r'space/(?P<spot_id>\d+)$', 'spot.SpotView'),
+    #url(r'space/(?P<spot_id>\d+)/json/$', 'spot.SpotView', {'return_json': True}),
+)
--- samsite/urls.py
+++ samsite/urls.py
@@ -1,11 +1,13 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'samsite.views.home', name='home'),
-    # url(r'^samsite/', include('samsite.foo.urls')),
-
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('sam.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
