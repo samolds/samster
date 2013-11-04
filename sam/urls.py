860a51af9ab6cb83ed58db6e8d8b4f25324651ce
Sam Olds <samolds@gmail.com>
2013-11-03 19:57:06 -0700

    I think i got everything set up on this new laptop for more dev
    for some reason a ton of .pyc and lib/ and bin/ stuff are showing
    up in a git status. hopefully it will get fixed with this commit
    and they will actaully be ignored. it seems like they're ignoring
    the .gitignore.

--- sam/urls.py
+++ sam/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('sam.views',
     url(r'^$', 'home.home'),
+    url(r'^around_the_web$', 'around_the_web.around_the_web'),
     #url(r'buildings?$', 'buildings.buildings'),
     #url(r'search/$', 'search.SearchView'),
     #url(r'contact(?:/(?P<spot_id>\d+))?/$', 'contact.contact'),
