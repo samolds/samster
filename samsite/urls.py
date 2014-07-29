bd745d26684c28be178652a880444522b05f124d
Sam Olds <samolds@gmail.com>
2014-07-29 12:27:16 -0600

    got the front page to load the api data asynchronously!
    now i just need to actually get the js variables to be displayed
    nicely. maybe i can set the django template variables to whatever
    the js variables are...

--- samsite/urls.py
+++ samsite/urls.py
@@ -2,11 +2,15 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
+from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+
+dajaxice_autodiscover()
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('sam.urls')),
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
 )
 
 handler403 = 'sam.views.error.err_403'
